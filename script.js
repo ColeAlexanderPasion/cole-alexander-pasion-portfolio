743c2af40a9393e7cf31cc6b5381b643e1a7a2ac
ColeAlexanderPasion <104624277+ColeAlexanderPasion@users.noreply.github.com>
2026-01-30 17:47:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    if (this.scrollY > 400) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["IT Graduate"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  $('.menu-btn').click(function () {
+    $('.menu').toggle();
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["IT Graduate"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  new Typed(".typing", {
+    strings: [
+      "IT Professional",
+      "Web Developer",
+      "Graphic Designer",
+      "Full-Stack Developer"
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+  });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    responsive: {
+      0: { items: 1 },
+      768: { items: 2 },
+      1000: { items: 3 }
+    }
+  });
+
 });
